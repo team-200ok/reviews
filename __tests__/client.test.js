bcd2ca01436899ca3b69b208c20ea3519b6ab6cc
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-12-04 15:21:26 -0800

    Installed Enzyme and fixed an issue with async functions in Enzyme-Jest

---
+++ __tests__/client.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { shallow } from 'enzyme';
+// import MyComponent from './MyComponent';
+
+// describe('<MyComponent />', () => {
+//   it('renders three <Foo /> components', () => {
+//     const wrapper = shallow(<MyComponent />);
+//     expect(wrapper.find(Foo)).to.have.lengthOf(3);
+//   });
+// });
+
+test('it adds two numbers', () => {
+  expect(1 + 1).toBe(2);
+});
