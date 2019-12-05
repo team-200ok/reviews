7b43f52cd482d4a338fbeb4cf54169dde2200311
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-12-04 17:21:21 -0800

    Added smoke tests for the three basic components: app, feed, and review

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -1,13 +1,31 @@
 import React from 'react';
-// import { shallow } from 'enzyme';
-// import MyComponent from './MyComponent';
+import { shallow } from 'enzyme';
+import App from '../client/app';
+import Feed from '../client/feed';
+import Review from '../client/review';
 
-// describe('<MyComponent />', () => {
-//   it('renders three <Foo /> components', () => {
-//     const wrapper = shallow(<MyComponent />);
-//     expect(wrapper.find(Foo)).to.have.lengthOf(3);
-//   });
-// });
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders at least one feed and review component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(<Feed />)).to.have.lengthOf(1);
+  });
+});
+
+describe('<Feed />', () => {
+  it('renders without crashing', () => {
+    shallow(<Feed />);
+  });
+});
+
+describe('<Review />', () => {
+  it('renders without crashing', () => {
+    shallow(<Review />);
+  });
+});
 
 test('it adds two numbers', () => {
   expect(1 + 1).toBe(2);
--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/newline-after-import */
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
