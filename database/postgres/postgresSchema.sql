c59062fc0b246c9b65bda5588cc0406c4f10a229
DanFein <danfeinstat@gmail.com>
2019-12-20 09:57:59 -0800

    updated readme + postgres schema

--- database/postgres/postgresSchema.sql
+++ database/postgres/postgresSchema.sql
@@ -10,17 +10,18 @@ DROP TABLE IF EXISTS business, users, reviews, comments, images;
 
 
 CREATE TABLE business (
-  b_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   business_name VARCHAR(20) NOT NULL,
   claimed BOOLEAN DEFAULT 'f',
   category VARCHAR(20),
   business_date DATE NOT NULL,
+ user_id INTEGER REFERENCES users(id),
   description TEXT
 );
 
 
 CREATE TABLE users (
-  u_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   first_name VARCHAR(15) NOT NULL,
   last_name VARCHAR(20),
   email VARCHAR(50) NOT NULL,
@@ -28,34 +29,36 @@ CREATE TABLE users (
   friend_count SMALLINT DEFAULT 0,
   review_count INTEGER DEFAULT 0,
   image_count INTEGER DEFAULT 0,
-  location VARCHAR(30) NOT NULL,
+  region VARCHAR(30),
+  city VARCHAR(30) NOT NULL,
   avatar_url VARCHAR(100) DEFAULT 'https://loremflickr.com/320/240'
 );
 
 CREATE TABLE reviews (
-  r_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   review_date DATE NOT NULL,
   review_text TEXT NOT NULL,
   star_count SMALLINT NOT NULL,
   useful_count INTEGER DEFAULT 0,
   funny_count INTEGER DEFAULT 0,
   cool_count INTEGER DEFAULT 0,
-  business_id INTEGER REFERENCES business(b_id),
-  user_id INTEGER REFERENCES users(u_id)
+  business_id INTEGER REFERENCES business(id),
+  user_id INTEGER REFERENCES users(id)
 );
 
 CREATE TABLE comments (
-c_id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 comment_date DATE NOT NULL,
 comment_text TEXT NOT NULL,
-parent_id INTEGER REFERENCES reviews(r_id),
-user_id INTEGER REFERENCES users(u_id)
+parent_id INTEGER REFERENCES reviews(id),
+user_id INTEGER REFERENCES users(id)
 );
 
 CREATE TABLE images (
-  i_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   image_url VARCHAR(100) NOT NULL,
+  image_date DATE NOT NULL,
   caption VARCHAR(80) NOT NULL,
-  reviews_id INTEGER REFERENCES reviews(r_id),
-  user_id INTEGER REFERENCES users(u_id)
+  reviews_id INTEGER REFERENCES reviews(id),
+  user_id INTEGER REFERENCES users(id)
 );
--- server/README.md
+++ server/README.md
@@ -1,13 +1,13 @@
 ### CRUD API
 
-Create/POST : /photo
+Create/POST : /photo:reviewId
 posts a new photo
 
 READ/GET : /business/:resterauntId
  gets joined table info for reviews (with associated users and images) tied to the specific business
 
-Update/PUT : /vote/cool/:resterauntId
-updates number of cool count of the image by +1 or -1
+Update/PUT : /vote/cool/:reviewId
+updates increments the cool count of the image
 
 Delete/DELETE : /review/:reviewId
 delete the review associated with the Id
